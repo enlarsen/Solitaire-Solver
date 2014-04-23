3cff025d5f9af2432b453c43d7e12d38b1aa7876
Erik Larsen <erik_larsen@icloud.com>
2014-04-23 09:27:44 -0700

    Initial Commit

---
+++ Solitaire-Solver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Solitaire-Solver
+//
+//  Created by Erik Larsen on 4/23/14.
+//  Copyright (c) 2014 Erik Larsen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
